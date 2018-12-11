d9f293dccfcd0a40cb14cd5ef953a5dfdea0c2b9
junior <juniornc81@gmail.com>
2018-12-10 22:48:31 -0500

    Readme file

---
+++ HomeworksNovember2018/home work dec2018/javahomework2/src/variables/Demo2.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class Demo2 {
+    int c = variablesDemo.z;
+
+    public void demo(){
+      int a= variablesDemo. z;
+
+    }
+}
---
+++ HomeworksNovember2018/home work dec2018/javahomework2/src/variables/variablesDemo.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class variablesDemo {
+//Instance variable
+    int x = 2;
+    //class variable
+    static int z = 4;
+
+    public void sum(){
+        int y =4;
+
+    }
+
+    public static void usingstatic(){
+        System.out.println("this is a static method");
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
